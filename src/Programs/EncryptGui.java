e80c446d8c3c1da1e5b7f17be108f2109a00dc10
TheMajorN <nickbaranosky@gmail.com>
2021-01-29 20:26:41 -0500

    Added Better Encryption Test

--- src/Programs/EncryptGui.java
+++ src/Programs/EncryptGui.java
@@ -6,14 +6,18 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 public class EncryptGui extends Application {
 
@@ -88,5 +92,41 @@ public class EncryptGui extends Application {
         } else {
             System.out.println("Nothing was typed");
         }
+    }
+
+    public static void encryptDecrypt(String key, int cipherMode, File in, File out)
+            throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IOException {
+
+        FileInputStream fis = new FileInputStream(in);
+        FileOutputStream fos = new FileOutputStream(out);
+
+        DESKeySpec desKeySpec = new DESKeySpec(key.getBytes());
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+        SecretKey secretKey = skf.generateSecret(desKeySpec);
+
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+
+        if (cipherMode == Cipher.ENCRYPT_MODE) {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, SecureRandom.getInstance("SHA1PRNG"));
+            CipherInputStream cis = new CipherInputStream(fis, cipher);
+            write(cis, fos);
+
+        } else if (cipherMode == Cipher.DECRYPT_MODE) {
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, SecureRandom.getInstance("SHA1PRNG"));
+            CipherOutputStream cos = new CipherOutputStream(fos, cipher);
+            write(fis, cos);
+        }
+    }
+
+    private static void write(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[64];
+        int numOfBytesRead;
+
+        while((numOfBytesRead = in.read(buffer)) != -1) {
+            out.write(buffer, 0, numOfBytesRead);
+        }
+        out.close();
+        in.close();
     }
 }
